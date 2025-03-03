e557286faa1aace1581c5b711dac078f7c35f889
Shanjjeev-Lab <shanjjeevrohan2911@gmail.com>
2025-03-03 15:31:19 +0530

    Aggregate ACP SQL

---
+++ SQLACP3/salary.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS Employee 
+(
+ID INT,
+NAME TEXT,
+ROLE TEXT,
+SALARY REAL
+);
+
+INSERT INTO Employee VALUES 
+(1, 'John Alter', 'Manufacturer', 50),
+(2, 'Sammy Koel', 'Baker', 40),
+(3, 'Micheol Keaton', 'Software Developer', 45,
+(4, 'Bandit Heeler', 'Business Analysis', 60),
+(5, 'Angelino Jewel', 'Manufacturer', 30),
+(6, 'Michelle Khare', 'Baker', 25);
